6d25b5f8babeca00d597dea05157011bc3ca1fcd
gyektai18 <gyektai18@deerfield.edu>
2016-01-10 22:58:37 -0500

    Add Some Scanners

--- ComputerCostReal/src/ElectricityComp.java
+++ ComputerCostReal/src/ElectricityComp.java
@@ -7,34 +7,50 @@ public class ElectricityComp
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		
-		/*
-		int allComputerCost = 1000;
-		int screenSize = 15;
-		String brandCPU;
-		brandCPU = "thisBrand";
-		int electricCPU;
-		int electricityHardDrive;
-		double mouseCost;
+
+		int allComputerCost;
+		Scanner initCost = new Scanner(System.in);
+		System.out.println("What was the cost of your Computer?");
+		allComputerCost = initCost.nextInt();
+		System.out.println(allComputerCost);
+		
+		int screenSize;
+		Scanner inchScreen = new Scanner(System.in);
+		System.out.println("What is the size of your screen in inches?");
+		screenSize = inchScreen.nextInt();
+		System.out.println(screenSize);
+	
+		int sizeHardDrive;
+		Scanner HardDrive = new Scanner(System.in);
+		System.out.println("How large is your Hard Drive?");
+		sizeHardDrive = HardDrive.nextInt();
+		System.out.println(sizeHardDrive);
+		
+		int mouseCost;
+		Scanner costMouse = new Scanner(System.in);
+		System.out.println("How much did your mouse cost?");
+		mouseCost = costMouse.nextInt();
+		System.out.println(mouseCost);
+		
 		int lengthComputerIsKept;
+		Scanner yearsKept = new Scanner(System.in);
+		System.out.println("How many years will you keep your computer?");
+		lengthComputerIsKept = yearsKept.nextInt();
+		System.out.println(lengthComputerIsKept);
+		
 		int hoursPerDay;
-		int screenCost;
-		hoursPerDay = 9;
-		int brandCPU;
-	*/
+		Scanner dailyHours = new Scanner(System.in);
+		System.out.println("How many hours will you use the Computer each Day?");
+		hoursPerDay = dailyHours.nextInt();
+		System.out.println(hoursPerDay);
 
 		String brandCPU;
-		Scanner sc = new Scanner(System.in);
+		Scanner CPU = new Scanner(System.in);
 		System.out.println("What CPU are you using?");
-		brandCPU = sc.nextLine();
+		brandCPU = CPU.nextLine();
 		System.out.println(brandCPU.toLowerCase());
 		
-		
-		/*
-		double wattage = mouseCost + electricityHardDrive + electricCPU + mouseCost;
-		double kWh = wattage * hoursPerDay; */
-		}
 		
 		
 		
@@ -45,4 +61,4 @@ public class ElectricityComp
 		
 		
 	}
-
+}
