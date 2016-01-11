c41f77fa04dd22e7918a5099f035a92c86c5e45a
gyektai18 <gyektai18@deerfield.edu>
2016-01-10 19:34:51 -0500

    Added code

---
+++ ComputerCostReal/src/ElectricityComp.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ElectricityComp
+
+
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		/*
+		int allComputerCost = 1000;
+		int screenSize = 15;
+		String brandCPU;
+		brandCPU = "thisBrand";
+		int electricCPU;
+		int electricityHardDrive;
+		double mouseCost;
+		int lengthComputerIsKept;
+		int hoursPerDay;
+		int screenCost;
+		hoursPerDay = 9;
+		int brandCPU;
+	*/
+
+		String brandCPU;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What CPU are you using?");
+		brandCPU = sc.nextLine();
+		System.out.println(brandCPU.toLowerCase());
+		
+		
+		/*
+		double wattage = mouseCost + electricityHardDrive + electricCPU + mouseCost;
+		double kWh = wattage * hoursPerDay; */
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
